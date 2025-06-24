8b3931a1dc99feae2c784182d89e74fac0a1a93e
michaelessiet <emsaa2002@gmail.com>
2025-06-24 10:45:17 +0100

    chore: lint

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,30 +1,30 @@
 import { PrivyClient } from "@privy-io/server-auth";
 import { cookies } from "next/headers";
-import { redirect, RedirectType } from "next/navigation";
+import { RedirectType, redirect } from "next/navigation";
 
 export default async function DashboardLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  const privyAuthToken = cookieStore.get("privy-token");
+	const cookieStore = await cookies();
+	const privyAuthToken = cookieStore.get("privy-token");
 
-  if (privyAuthToken) {
-    const client = new PrivyClient(
-      process.env.NEXT_PUBLIC_PRIVY_APP_ID as string,
-      process.env.PRIVY_APP_SECRET as string,
-    );
+	if (privyAuthToken) {
+		const client = new PrivyClient(
+			process.env.NEXT_PUBLIC_PRIVY_APP_ID as string,
+			process.env.PRIVY_APP_SECRET as string,
+		);
 
-    try {
-      await client.verifyAuthToken(privyAuthToken.value);
-    } catch (e) {
-      console.error(e);
-      redirect("/", RedirectType.replace);
-    }
-  } else {
-    redirect("/", RedirectType.replace);
-  }
+		try {
+			await client.verifyAuthToken(privyAuthToken.value);
+		} catch (e) {
+			console.error(e);
+			redirect("/", RedirectType.replace);
+		}
+	} else {
+		redirect("/", RedirectType.replace);
+	}
 
-  return <>{children}</>;
+	return <>{children}</>;
 }
--- components/providers.tsx
+++ components/providers.tsx
@@ -4,26 +4,26 @@ import { PrivyProvider } from "@privy-io/react-auth";
 import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
-      config={{
-        appearance: {
-          showWalletLoginFirst: true,
-          walletChainType: "solana-only",
-        },
-        loginMethods: ["wallet", "email"],
-        externalWallets: {
-          solana: {
-            connectors: toSolanaWalletConnectors(),
-          },
-        },
-        embeddedWallets: {
-          createOnLogin: "all-users",
-        },
-      }}
-    >
-      {children}
-    </PrivyProvider>
-  );
+	return (
+		<PrivyProvider
+			appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
+			config={{
+				appearance: {
+					showWalletLoginFirst: true,
+					walletChainType: "solana-only",
+				},
+				loginMethods: ["wallet", "email"],
+				externalWallets: {
+					solana: {
+						connectors: toSolanaWalletConnectors(),
+					},
+				},
+				embeddedWallets: {
+					createOnLogin: "all-users",
+				},
+			}}
+		>
+			{children}
+		</PrivyProvider>
+	);
 }
